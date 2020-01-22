0d255c178dea06badc9b9580ca2c3ea708f79c91
omidnb <nobahariomid@gmail.com>
2020-01-22 13:21:03 -0500

    work with operators

---
+++ S01/basic_operators.py
@@ -0,0 +1,18 @@
+num_1 = 10
+num_2 = 100
+
+num_3 = num_1 + num_2
+num_4 = num_3 * num_2
+
+print(num_3)
+print(num_4)
+
+#string operators
+word_1 = "Hello "
+word_2 = "World."
+
+text = word_1 + word_2
+print(text)
+
+paragraph = text * 3
+print(paragraph)
