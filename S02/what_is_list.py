daea84973057df7be2960f1164f893106ed23b12
omidnb <nobahariomid@gmail.com>
2020-01-28 11:39:25 -0500

    getting started with conditional statements

---
+++ S02/what_is_list.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+print(type(my_list))
+
+length_of_my_list = len(my_list)
+print("the length of 'my_list' is", length_of_my_list, "")
+
+print("first element of 'my_list' is", my_list[0])
+print("last element of 'my_list' is", my_list[-1])
+
+print("the average of 'my_list' is", sum(my_list) / length_of_my_list)
