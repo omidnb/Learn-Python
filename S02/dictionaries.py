aa9ed6da719a1e59e14e03b2db3bca6c5c2471ff
omidnb <nobahariomid@gmail.com>
2020-02-04 00:47:23 -0500

    dictionary

---
+++ S02/dictionaries.py
@@ -0,0 +1,10 @@
+# **** tuple(), dict{}, list[] ****
+
+lst = ["one", "two", "three"]                         # list
+dct = {0: "one", 1: "two", 2: "three"}                # dictionary
+
+song = {"name": "Casablanca", "singer": "Bertie Higgins", "lang": "ENG"}
+print(song["singer"])
+
+song["playlist"] = "myFav"
+print(song)
