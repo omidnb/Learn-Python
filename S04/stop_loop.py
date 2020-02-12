094791bc9e00bc190b1091e823bf3a279a5554c5
omidnb <nobahariomid@gmail.com>
2020-02-12 10:53:05 -0500

    fibonacci by for and using range

--- S04/stop_loop.py
+++ S04/stop_loop.py
@@ -9,11 +9,17 @@ else:
     print("this 'else' is for 'while'")  # instead of if break
 
 
-# fibonacci
-limit = 20
-list_fib = [1, 1]
+# # fibonacci
+# limit = 20
+# list_fib = [1, 1]
+#
+# while len(list_fib) < limit:
+#     new_element = list_fib[-1] + list_fib[-2]
+#     list_fib.append(new_element)
+# print(list_fib)
 
-while len(list_fib) < limit:
-    new_element = list_fib[-1] + list_fib[-2]
-    list_fib.append(new_element)
-print(list_fib)
+fib = [1, 1]
+for x in range(10):
+    res = fib[-1] + fib[-2]
+    fib.append(res)
+    print(res)
