945d09fb0cf321af34b81f440de630a6ea2dfb92
omidnb <nobahariomid@gmail.com>
2020-02-13 13:28:16 -0500

    little update

--- S05/range.py
+++ S05/range.py
@@ -15,3 +15,12 @@ print(next(x))
 
 for value in range(5, 10, 2):  # from 5 to 10, 2 by 2
     print("*", value)
+
+# another example
+start = 0
+stop = 100
+step = 20
+
+for i in range(start, stop, step):
+    print("hello", i)
+
